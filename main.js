09273df47a3617a5924417fe50ff48d61f2ff50b
Julian van Doorn <jvdoorn@antarc.com>
2019-01-19 15:51:38 +0100

    Reindentation and use native frame.

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function createWindow() {
         width: 1220,
         height: 800,
         show: false,
-        frame: false,
+        frame: true,
         icon: path.join(__dirname, 'img/icons/icon@64px.png')
     });
 
